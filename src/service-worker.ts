117d46d9be83fd15c6f5a1f2cb49f198893134f2
Tevfik M Sertel <tevfik@tevfik-sertel.myfiosgateway.com>
2020-05-07 16:58:58 -0400

    Working on deep fetching

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,17 +9,24 @@ new Prefetcher({
   plugins: [
     new DeepFetchPlugin([
       {
-        selector: 'img.product-featured-media',
+        selector: 'img.product-main-image',
         maxMatches: 1,
         attribute: 'src',
         as: 'image',
+        // callback: logPrefetchedContent,
       },
       {
-        selector: 'img.grid-view-item__image',
-        maxMatches: 4,
+        selector: '#mobile-product-images #product-thumbnails .lazyload-blur-wrapper>img',
+        maxMatches: 1,
         attribute: 'src',
         as: 'image',
         callback: deepFetchResponsiveImages,
+      },
+      {
+        selector: '.indiv-product img',
+        maxMatches: 2,
+        attribute: 'src',
+        as: 'image',
       },
     ]),
   ],
@@ -29,14 +36,20 @@ new Prefetcher({
 
 function deepFetchResponsiveImages({ $el, el, $ }: DeepFetchCallbackParam) {
   const urlTemplate = $el.attr('data-src')
-  const dataWidths = $el.attr('data-widths')
+  // const dataWidths = $el.attr('data-widths')
+  const width = "900"
+  if (urlTemplate && width) {
+    // const widths = JSON.parse(dataWidths)
+    // for (let width of widths.slice(0, 2)) {
+    //   const url = urlTemplate?.replace(/\{width\}/, width)
+    //   prefetch(url, 'image')
+    // }
 
-  if (dataWidths && urlTemplate) {
-    const widths = JSON.parse(dataWidths)
-
-    for (let width of widths.slice(0, 2)) {
-      const url = urlTemplate?.replace(/\{width\}/, width)
-      prefetch(url, 'image')
-    }
+    const url = urlTemplate.replace(/\{width\}/,width)
+    prefetch(url, 'image')
   }
+}
+
+function logPrefetchedContent({$el}) { // for testing
+  console.log("content '"+$el.attr('src')+"' has been prefetched...")
 }
--- src/transform.ts
+++ src/transform.ts
@@ -2,7 +2,7 @@ import cheerio from 'cheerio'
 
 export default function transform(response) {
   const $ = cheerio.load(response.body)
-
+    // console.log("Transform script running on '"+response.req.originalUrl+"'") // for testing
   $('head').append(`
     <script src="/__xdn__/cache-manifest.js" defer="defer"></script>
     <script src="/main.js" defer="defer"></script>
